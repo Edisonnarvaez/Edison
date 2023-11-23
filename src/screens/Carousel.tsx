5b19c37c3366495c79caafb056660de78eff0181
Edison <edison.narvaez@campusucc.edu.co>
2023-11-23 13:59:46 -0500

    actualizacion 6

--- src/screens/Carousel.tsx
+++ src/screens/Carousel.tsx
@@ -1,7 +1,4 @@
-
-
-import {FaGithub} from 'react-icons/fa6';
-
+import { FaGithub } from 'react-icons/fa6';
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -18,40 +15,41 @@ const Carousel: React.FC = () => {
 
   return (
     <Slider {...settings}>
-      <div>
-        
-        <img src="./public/slide1.jpg" alt="Slide 1" />
-        <section className="flex-1 grid grid-flow-row-[25px_40_px] items-center text-white p-4">
-          <h2 className='text-4xl font-roboto flex justify-center text-electric-violet-700'>Patrones De Software</h2>
-          <p className="flex justify-center">En este repositorio se encuentran varios proyectos en java cuyo propósito es la implementación de patrones de software.</p>
-          <li>
-            <button className='flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700'><FaGithub className='mr-2' /><span className='text-black'>Github</span></button>
-          </li>
+      <section className="flex-1 h-[375px] justify-center">
+        <img src="./public/slide1.jpg" alt="Slide 1" className="w-full h-full object-cover" />
+          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
+          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
+          <section className="flex justify-center">
+            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
+              <FaGithub className="mr-2" />
+              <span className="text-black">Github</span>
+            </button>
         </section>
-      </div>
-      <div>
-        
-        <img src="./public/slide2.jpg" alt="Slide 2" />
-        <section className="flex-1 grid grid-flow-row-[25px_40_px] items-center text-white p-4">
-          <h2 className='text-4xl font-roboto flex justify-center text-electric-violet-700'>Desarrollo web</h2>
-          <p className="flex justify-center">En este repositorio se encuentran varios ejercicios de lógica de programación en Python</p>
-          <li>
-            <button className='flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700'><FaGithub className='mr-2' /><span className='text-black'>Github</span></button>
-          </li>
+      </section>
+
+      <section className="flex-1 h-[375px] justify-center">
+        <img src="./public/slide1.jpg" alt="Slide 2" className="w-full h-full object-cover" />
+          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
+          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
+          <section className="flex justify-center">
+            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
+              <FaGithub className="mr-2" />
+              <span className="text-black">Github</span>
+            </button>
         </section>
-      </div>
-      <div>
-        
-        <img src="./public/slide3.jpg" alt="Slide 3" />
-        <section className="flex-1 grid grid-flow-row-[25px_40_px] items-center text-white p-4">
-          <h2 className='text-4xl font-roboto flex justify-center text-electric-violet-700'>Portafolio</h2>
-          <p className="flex justify-center">En este repositorio encontrara el codigo del portafolio utilizando Vite + Tailwind</p>
-          <li>
-            <button className='flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700'><FaGithub className='mr-2' /><span className='text-black'>Github</span></button>
-          </li>
+      </section>
+
+      <section className="flex-1 h-[375px] justify-center">
+        <img src="./public/slide1.jpg" alt="Slide 3" className="w-full h-full object-cover" />
+          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
+          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
+          <section className="flex justify-center">
+            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
+              <FaGithub className="mr-2" />
+              <span className="text-black">Github</span>
+            </button>
         </section>
-      </div>
-      
+      </section>
     </Slider>
   );
 };
--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -3,9 +3,9 @@ import Carousel from '../screens/Carousel';
 
 const Projects = () => {
   return (
-    <section className='flex flex-col bg-bunker-950 mb-10 p-4'>
+    <section className='flex flex-col w-[750px] h-[800px] bg-bunker-950 mb-10 p-4'>
       <h1 className='text-8xl font-roboto flex justify-center text-electric-violet-700'>Proyectos</h1>
-      <section className="flex-1 flex items-center">
+      <section>
         <Carousel />
       </section>
     </section>
