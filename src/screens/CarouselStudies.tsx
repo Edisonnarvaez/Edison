675b2311092e0a2c0401b87383828b5e5169d404
Edison <edison.narvaez@campusucc.edu.co>
2023-11-23 18:37:16 -0500

    studies

--- src/screens/CarouselStudies.tsx
+++ src/screens/CarouselStudies.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from 'react-icons/fa6';
+
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -15,39 +15,33 @@ const CarouselStudies: React.FC = () => {
 
   return (
     <Slider {...settings}>
-      <section className="flex-1 h-[375px] justify-center p-2">
-        <img src="./public/slide1.jpg" alt="Slide 1" className="w-full h-full object-cover" />
-          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
-          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
-          <section className="flex justify-center">
-            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
-              <FaGithub className="mr-2" />
-              <span className="text-black">Github</span>
-            </button>
+      <section className="flex-1 h-[375px] p-12 bg-gray-800 flex flex-col items-center justify-center mx-auto">
+
+        <p className="flex justify-center text-app-white">Bachiller técnico automotriz.</p>
+        <h2 className="text-6xl font-roboto flex justify-center text-white">ITSIM</h2>
+        <p className="flex justify-center text-app-white">2012 - 2015</p>
+        <section className="flex justify-center items-center">
+          <img src="./public/itsim.png" alt="itsim 1" className="object-cover max-w-full max-h-full" />
         </section>
       </section>
 
-      <section className="flex-1 h-[375px] justify-center p-2">
-        <img src="./public/slide1.jpg" alt="Slide 2" className="w-full h-full object-cover" />
-          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
-          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
-          <section className="flex justify-center">
-            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
-              <FaGithub className="mr-2" />
-              <span className="text-black">Github</span>
-            </button>
+      <section className="flex-1 h-[375px] p-12 bg-gray-800 flex flex-col items-center justify-center mx-auto">
+
+        <p className="flex justify-center text-app-white">Técnico de sistemas</p>
+        <h2 className="text-6xl font-roboto flex justify-center text-white">CETEM</h2>
+        <p className="flex justify-center text-app-white">2018 - 2019</p>
+        <section className="flex justify-center items-center">
+          <img src="./public/cetem.png" alt="itsim 1" className="object-cover max-w-full max-h-full" />
         </section>
       </section>
 
-      <section className="flex-1 h-[375px] justify-center p-2">
-        <img src="./public/slide1.jpg" alt="Slide 3" className="w-full h-full object-cover" />
-          <h2 className="text-4xl font-roboto flex justify-center text-electric-violet-700">Patrones De Software</h2>
-          <p className="flex item-center text-app-white">En este repositorio se encuentran varios proyectos en Java cuyo propósito es la implementación de patrones de software.</p>
-          <section className="flex justify-center">
-            <button className="flex items-center text-gray-300 bg-gray-800 p-2 rounded border-electric-violet-700">
-              <FaGithub className="mr-2" />
-              <span className="text-black">Github</span>
-            </button>
+      <section className="flex-1 h-[375px] p-12 bg-gray-800 flex flex-col items-center justify-center mx-auto">
+
+        <p className="flex justify-center text-app-white">Ingeniero de software</p>
+        <h2 className="text-6xl font-roboto flex justify-center text-white">UCC</h2>
+        <p className="flex justify-center text-app-white">2021 - 2025</p>
+        <section className="flex justify-center items-center">
+          <img src="./public/ucc.png" alt="itsim 1" className="object-cover max-w-full max-h-full" />
         </section>
       </section>
     </Slider>
